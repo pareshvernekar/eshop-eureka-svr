623e8991985b4a7063d153c267237f5633140460
Paresh Vernekar <pvernekar@Avenues-MacBook-Pro-4.local>
2018-08-17 14:51:41 -0700

    Initial checkin

---
+++ src/main/java/test/eshop/eureka/EshopEurekaSvrApplication.java
@@ -0,0 +1,14 @@
+package test.eshop.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EshopEurekaSvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopEurekaSvrApplication.class, args);
+	}
+}
